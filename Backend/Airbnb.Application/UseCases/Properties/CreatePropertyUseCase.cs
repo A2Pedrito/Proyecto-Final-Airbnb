783bbe11d58c4ab1cf5fc54ca096850ab58cd694
Pedro E. <pedroemmanuel540@gmail.com>
2026-04-17 01:23:24 -0400

    Cambios .

---
+++ Backend/Airbnb.Application/UseCases/Properties/CreatePropertyUseCase.cs
@@ -0,0 +1,48 @@
+﻿using Airbnb.Application.DTOs.Property;
+using Airbnb.Domain.Entities;
+using Airbnb.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airbnb.Application.UseCases.Properties
+{
+    public class CreatePropertyUseCase
+    {
+        private readonly IPropertyRepository _propertyRepository;
+
+        public CreatePropertyUseCase(IPropertyRepository propertyRepository)
+        {
+            _propertyRepository = propertyRepository;
+        }
+
+        public async Task<PropertyResponse> ExecuteAsync(CreatePropertyRequest request, Guid hostId)
+        {
+            var property = new Property
+            {
+                Id = Guid.NewGuid(),
+                Title = request.Title,
+                Description = request.Description,
+                Location = request.Location,
+                PricePerNight = request.PricePerNight,
+                Capacity = request.Capacity,
+                HostId = hostId // <--- Asignamos el ID seguro que viene del Token
+            };
+
+            await _propertyRepository.AddAsync(property);
+
+            return new PropertyResponse
+            {
+                Id = property.Id,
+                Title = property.Title,
+                Description = property.Description, // Agregado porque lo pusiste en tu DTO
+                Location = property.Location,
+                PricePerNight = property.PricePerNight,
+                Capacity = property.Capacity,
+                HostId = property.HostId
+            };
+        }
+    }
+}
